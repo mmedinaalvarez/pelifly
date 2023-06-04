74fad863585562e8696c73405cbd73a0211ba7a5
Manuel Medina <manuelmedinaalvarez@gmail.com>
2023-06-04 19:49:01 -0300

    arreglo de navbar ahora integrado uno de mui y creo componente itemlistcontainer para colocarlo en el app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import "./App.css";
 
 // Components
 import Header from "./components/Header/Header";
-import CardMovie from './components/CardMovie/CardMovie';
 import NavBar from './components/NavBar/NavBar';
+import ItemListcontainer from './components/ItemListContainer/ItemListcontainer';
 
 
 
@@ -16,26 +16,7 @@ class App extends Component{
         <Header title="Pelifly" 
         />
         <NavBar />
-        <div className='UserSection'>
-          <CardMovie 
-            name="Blondi"
-            date="Estreno"
-            description="Es una película que, en tono de comedia, abre imperceptiblemente temas enormes y trascendentes. Mientras relata la historia particular y cotidiana del vínculo de una madre con su hijo."
-            img="../src/assets/img/movies/blondi.jpg"  
-          />
-          <CardMovie 
-            name="Disco de oro"
-            date="Se unio en Enero de 2023"
-            description="Biopic del referente musical Neil Bogart, un soñador judío que, empezando con muchas ganas y poco dinero,"
-            img="../src/assets/img/movies/discodeoro.jpg"  
-          />
-          <CardMovie 
-            name="SPIDERMAN A TRAVES DEL SPIDERVERSO"
-            date="Estreno"
-            description="Después de reunirse con Gwen Stacy, Spider-Man, el amigable vecino de Brooklyn, es transportado a través del Multiverso."
-            img="../src/assets/img/movies/spiderman.jpg"
-          />
-        </div>
+        <ItemListcontainer />
       </div>
     );
   }
---
+++ src/components/ItemListContainer/ItemListcontainer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import CardMovie from '../CardMovie/CardMovie';
+
+
+const ItemListContainer = () => {
+  return (
+    <div className='UserSection'>
+          <CardMovie 
+            name="Blondi"
+            date="Estreno"
+            description="Es una película que, en tono de comedia, abre imperceptiblemente temas enormes y trascendentes. Mientras relata la historia particular y cotidiana del vínculo de una madre con su hijo."
+            img="../src/assets/img/movies/blondi.jpg"  
+          />
+          <CardMovie 
+            name="Disco de oro"
+            date="Estreno"
+            description="Biopic del referente musical Neil Bogart, un soñador judío que, empezando con muchas ganas y poco dinero,"
+            img="../src/assets/img/movies/discodeoro.jpg"  
+          />
+          <CardMovie 
+            name="SPIDERMAN A TRAVES DEL SPIDERVERSO"
+            date="Estreno"
+            description="Después de reunirse con Gwen Stacy, Spider-Man, el amigable vecino de Brooklyn, es transportado a través del Multiverso."
+            img="../src/assets/img/movies/spiderman.jpg"
+          />
+        </div>
+  )
+}
+
+export default ItemListContainer;
