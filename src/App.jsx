4a32d0bd52f3d7fd4ab0a4f87b19e23f912bf494
Manuel Medina <manuelmedinaalvarez@gmail.com>
2023-06-08 19:15:19 -0300

    vuelvo agregar navbar propio para un mejor manejo de los links

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./App.css";
-
 
 // Components
 import Header from "./components/Header/Header";
-import NavBar from './components/NavBar/NavBar';
-import ItemListcontainer from './components/ItemListContainer/ItemListcontainer';
+import NavBar from "./components/NavBar/NavBar";
 
+import ItemListcontainer from "./components/ItemListContainer/ItemListcontainer";
 
-
-class App extends Component{
-  render(){
-    return(
-      <div className='App'>
-        <Header title="Pelifly" 
-        />
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header title="Pelifly" />
         <NavBar />
         <ItemListcontainer />
       </div>
