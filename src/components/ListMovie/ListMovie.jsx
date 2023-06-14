79cc3cf767582b207937046bd565abb2719ce47f
Manuel Medina <manuelmedinaalvarez@gmail.com>
2023-06-14 20:00:59 -0300

    creo api url en env tambien

--- src/components/ListMovie/ListMovie.jsx
+++ src/components/ListMovie/ListMovie.jsx
@@ -3,14 +3,11 @@ import axios from "axios";
 import CardMovie from "../CardMovie/CardMovie";
 import "./ListMovie.css";
 
-// const API_KEY = process.env.REACT_APP_API_KEY;
-// console.log(import.meta.env.VITE_API);
-
 const ListMovie = () => {
   const [names, setNames] = useState([]);
   useEffect(() => {
     axios(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${
+      `${import.meta.env.VITE_API_URL}${
         import.meta.env.VITE_API_KEY
       }&language=es-ES`
     ).then((json) => setNames(json.data.results));
