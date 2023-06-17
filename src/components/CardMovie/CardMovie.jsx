3daf47c4627d33d489e202a412063e780a9dd54d
Manuel Medina <manuelmedinaalvarez@gmail.com>
2023-06-17 09:36:55 -0300

    termino la detailpage

--- src/components/CardMovie/CardMovie.jsx
+++ src/components/CardMovie/CardMovie.jsx
@@ -4,7 +4,6 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
-import { Height, Margin } from "@mui/icons-material";
 
 const CardMovie = ({ name }) => {
   return (
--- src/components/ListMovie/ListMovie.jsx
+++ src/components/ListMovie/ListMovie.jsx
@@ -9,7 +9,7 @@ const ListMovie = () => {
   const [names, setNames] = useState([]);
   useEffect(() => {
     axios(
-      `${import.meta.env.VITE_API_URL}${
+      `${import.meta.env.VITE_API_URL}now_playing${
         import.meta.env.VITE_API_KEY
       }&language=es-ES`
     ).then((json) => setNames(json.data.results));
